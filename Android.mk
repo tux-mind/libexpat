697d88174b48810121ddc526e230817177297e4b
tux-mind <massimo.dragano@gmail.com>
2014-05-27 01:26:12 +0200

    changed Android.mk to be more portable and clean

--- Android.mk
+++ Android.mk
@@ -1,14 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS:= -Wall -Wmissing-prototypes -Wstrict-prototypes -fexceptions -ffunction-sections -fdata-sections -DHAVE_EXPAT_CONFIG_H
+LOCAL_CFLAGS := -Wall -Wmissing-prototypes -Wstrict-prototypes -fexceptions \
+                -ffunction-sections -fdata-sections -DHAVE_EXPAT_CONFIG_H
 
-LOCAL_CXXFLAGS:=-Wall -fexceptions -ffunction-sections -fdata-sections
+LOCAL_C_INCLUDES:= \
+					$(LOCAL_PATH)\
+					$(LOCAL_PATH)/lib
 
-LOCAL_C_INCLUDES:=	expat\
-					expat/lib
-
-LOCAL_SRC_FILES:= 	lib/xmlparse.c\
+LOCAL_SRC_FILES:= \
+					lib/xmlparse.c\
 					lib/xmltok.c\
 					lib/xmlrole.c
 					
